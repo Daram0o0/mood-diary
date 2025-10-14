b7153ae6ffc4571851f0ef478bb0c237f2e81356
Daram0o0 <c30728535@gmail.com>
2025-10-14 16:50:33 +0900

    feat: navigation 영역 스타일 개선 및 피그마 디자인 적용
    - navigation 영역에 16px gap 추가
    - 사진보관함 탭의 border-bottom 제거
    - header와 footer의 좌우 padding 제거
    - navTabInactive 클래스 추가로 비활성 탭 스타일 분리
    - 피그마 디자인과 일치하도록 레이아웃 조정

--- src/commons/layout/index.tsx
+++ src/commons/layout/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from './styles.module.css';
 
 interface LayoutProps {
@@ -9,19 +10,32 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className={styles.layout}>
       <header className={styles.header}>
-        {/* Header content */}
+        <div className={styles.logo}>
+          <span className={styles.logoText}>민지의 다이어리</span>
+        </div>
       </header>
       
-      <div className={styles.gap}></div>
-      
       <div className={styles.banner}>
-        {/* Banner content */}
+        <Image
+          src="/images/banner.png"
+          alt="배너 이미지"
+          width={1168}
+          height={240}
+          className={styles.bannerImage}
+        />
       </div>
       
       <div className={styles.gap}></div>
       
       <nav className={styles.navigation}>
-        {/* Navigation content */}
+        <div className={styles.navContainer}>
+          <div className={styles.navTab}>
+            <span className={styles.navTabText}>일기보관함</span>
+          </div>
+          <div className={styles.navTabInactive}>
+            <span className={styles.navTabTextInactive}>사진보관함</span>
+          </div>
+        </div>
       </nav>
       
       <main className={styles.main}>
@@ -29,7 +43,11 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       </main>
       
       <footer className={styles.footer}>
-        {/* Footer content */}
+        <div className={styles.footerContent}>
+          <div className={styles.footerTitle}>민지의 다이어리</div>
+          <div className={styles.footerInfo}>대표 : 민지</div>
+          <div className={styles.footerCopyright}>Copyright © 2024. 민지 Co., Ltd.</div>
+        </div>
       </footer>
     </div>
   );
