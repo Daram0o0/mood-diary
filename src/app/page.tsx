e2eface249ddc9456fcbe5dec16eaf296f410bf9
Daram0o0 <c30728535@gmail.com>
2025-10-14 18:07:34 +0900

    feat(redirect): 루트에서 /diaries로 리디렉트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,5 @@
-'use client'
-import Button from "@/commons/components/button";
-import Input from "@/commons/components/input";
-import Pagination from "@/commons/components/pagination";
-import Searchbar from "@/commons/components/searchbar";
-import Toggle from "@/commons/components/toggle";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-
-  return ( 
-    <div className="flex flex-row items-center justify-center gap-20">
-      <div>
-        <label>버튼: </label>
-        <Button>버튼</Button>
-      </div>
-      <div>
-        <label>인풋: </label>
-        <Input/>
-      </div>
-      <div>
-        <label>페이지네이션: </label>
-        <Pagination currentPage={2} totalPages={10} onPageChange={()=>{}} />
-      </div>
-      <div>
-        <label>서치바: </label>
-        <Searchbar/>
-      </div>
-      <div>
-        <label>토글: </label>
-        <Toggle/>
-      </div>
-    </div>
-  );
+  redirect("/diaries");
 }
